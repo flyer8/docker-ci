04d554bbf10d3301dc08de3531e35d2fb257f5e8
Sergey Shimanskiy <flyer8@yandex.ru>
2017-07-13 16:18:08 +0300

    pom.xml fixed

--- java.groovy
+++ java.groovy
@@ -4,7 +4,7 @@ node('jenkins-slave') {
 stage 'Running maven for building'
    docker.image('flyer8/maven-git').inside {
    git 'https://github.com/flyer8/webapp1.git'
-   sh 'mvn -B clean install'
+   sh 'mvn -B clean deploy'
 
 stage 'Deploying to Tomcat apps server'
    sh 'curl -T "target/webapp1.war" "http://tomcat:tomcat@192.168.0.104:8888/manager/text/deploy?path=/webapp1&update=true"'
